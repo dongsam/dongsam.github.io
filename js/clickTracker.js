4f9fbbd7b0ed8cb3b864be8c81a61fd56a0a3c40
dongsamb <dongsamb@gmail.com>
2015-12-31 18:08:39 +0900

    add first post and modify font size and clickTracker

--- js/clickTracker.js
+++ js/clickTracker.js
@@ -11,9 +11,126 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 // need to check version and collision of jquery on target web service
 
 // version 0.1
-var serverUrl = 'http://administrator.co.kr:9999'; // temporary RESTful API Server
+var serverUrl = 'http://{{service-name}}-{{eb_version_lable_suffix}}.elasticbeanstalk.com'; // input your settinged RESTful API Server info
 var token = "VkdzNVRsRlZVa1JVTURWUFVsVk9WVk5WT1U4PQ==";  // hard coded temporary service token for olley
 var loaded = Date.now();
+
+function initQueue(){
+	// LocalStorage 지원여부 체크 및 기존에 저장된 큐가 있다면 가져오기
+	if(typeof(Storage) !== "undefined") {
+		var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	} else {
+		console.log("Local Storage not support");
+		return false;
+	}
+	console.log("initQueue 1");
+	console.log(LocalLogQueue);
+	// 위에서 가져와진 큐가 존재하는지 체크, 없다면 새로 생성하여 localStorage에 초기화
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		var LocalLogQueue = [];
+		console.log("initQueue 2");
+		console.log(LocalLogQueue);
+		localStorage.setItem("LocalLogQueue", JSON.stringify(LocalLogQueue));
+	}
+}
+
+function pushQueue(json_data){
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		return addQueue(json_data);
+	}
+	LocalLogQueue.push(json_data);
+	localStorage.setItem("LocalLogQueue", JSON.stringify(LocalLogQueue));
+	console.log("addQueue 3");
+	console.log(LocalLogQueue);
+	return true;
+}
+
+function popQueue(){
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		console.log("popQueue, empty, init");
+		return false;
+	}
+	else{
+		var json_data = LocalLogQueue.pop();
+		localStorage.setItem("LocalLogQueue", JSON.stringify(LocalLogQueue));
+		console.log("popQueue 2");
+		// console.log(LocalLogQueue);
+		if(typeof(json_data) == "undefined"){
+			return false;
+		}
+		else{
+			return json_data;
+		}
+	}
+}
+
+function shiftQueue(){
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		console.log("popQueue, empty, init");
+		return false;
+	}
+	else{
+		var json_data = LocalLogQueue.shift();
+		localStorage.setItem("LocalLogQueue", JSON.stringify(LocalLogQueue));
+		console.log("popQueue 2");
+		// console.log(LocalLogQueue);
+		if(typeof(json_data) == "undefined"){
+			return false;
+		}
+		else{
+			return json_data;
+		}
+	}
+}
+
+function getAllQueue(){
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	console.log("getAllQueue 1");
+	console.log(LocalLogQueue);
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		console.log("getAllQueue, empty, init");
+		return false;
+	}
+	else{
+		while(true){
+			var json_data = popQueue();
+			if(json_data){
+				console.log(json_data);
+			}
+			else{
+				console.log("getAllQueue, empty");
+				break;
+			}
+		}
+	}
+	return true;
+}
+
+function isEmptyQueue(){
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		console.log("checkEmptyQueue, empty, init");
+		return true;
+	}
+	else{
+		console.log("checkEmptyQueue, queue length:");
+		console.log(LocalLogQueue.length);
+		if(LocalLogQueue.length == 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
 
 // for make user_key , len == 20
 function make_random_key()
@@ -63,17 +180,16 @@ function makeLog(e, link, init){
 	console.log("make log 1");
 	init = typeof init !== 'undefined' ? init : false;
 	stored_link = link;
-	if(link == undefined){
+	if(link == undefined || link === true || link === false ){
 		link = "";
 	}
 	var spent_milli_sec = Date.now() - loaded;
-    var load_count = Number(localStorage.getItem("load_count"));
-    load_count += 1;
+	var load_count = Number(localStorage.getItem("load_count"));
+	load_count += 1;
 	localStorage.setItem("load_count", load_count);
 	click_data = clickHandleEvent(e);
 	// var data= 'link=' + $(this).attr("href") + '&spent_milli_sec=' + spent_milli_sec + '&timestamp=' + Date.now()
-	var now = new Date();
-	var now_ISO = now.toISOString();
+	var now_ISO = new Date().toISOString();
 	var json_data = {
 		timestamp: now_ISO,
 		token: token,
@@ -87,38 +203,66 @@ function makeLog(e, link, init){
 		load_count:load_count
 	}
 	if ( init == true ){
-        localStorage.setItem("json_data", JSON.stringify(json_data));
+		initQueue();
+		pushQueue(json_data);
 		return;
 	}
-	
-//	console.log(json_data);
-	if (typeof(Storage) !== "undefined") {
-		localStorage.setItem("json_data", JSON.stringify(json_data));
-	} else {
-		console.log("not support");
-	}
-//	console.log(JSON.parse(localStorage.getItem("json_data")));
+	pushQueue(json_data);
 	console.log("make log 2");
 }
 
-function sendLog(link){
+function sendAllLog(tmpLink){
+	if(typeof(tmpLink) == "undefined" || tmpLink == null){
+		tmpLink = "sendAllLog";
+	}
+	else{
+		tmpLink = tmpLink+" _ sendAllLog";
+	}
+	//flush queue
+	var LocalLogQueue = JSON.parse(localStorage.getItem("LocalLogQueue"));
+	console.log("sendAllLog flush queue 1");
+	console.log(LocalLogQueue);
+	if(LocalLogQueue === null || LocalLogQueue === ""){
+		initQueue();
+		console.log("sendAllLog flush queue, empty, init");
+		return false;
+	}
+	else{
+		var logCount = LocalLogQueue.length;
+		console.log("logCount "+logCount);
+		while(logCount >= 1){
+			console.log("flusing "+logCount);
+			// do not flush, only send
+			sendLog(link=tmpLink,flush=false);
+			logCount--;
+		}
+	}
+}
+
+function sendLog(link, flush){
 	stored_link = link;
-//	link = typeof(link) !== "undefined" ? link : "";
-//	link = link !== null ? link : "";
-    if(typeof(link) == "undefined" || link == null){
+	if(typeof(link) == "undefined" || link == null){
 		link = "";
 	}
-
+	if(typeof(link) == "undefined" || link == null){
+		flush = false;
+	}
 	console.log("send log");
-	var json_data = localStorage.getItem("json_data");
-	json_data = JSON.parse(json_data);
-	if( json_data === null ){
+
+	var json_data = popQueue();
+	if(!json_data){
 		console.log("log init");
 		makeLog(link=document.location.href, init=true);
+		// check below line
+		sendLog(link=document.location.href);
 	}
-    console.log(json_data);
-	json_data["link"] = link;
-
+	if(!json_data["link"]){
+		json_data["link"] = link;
+	}else{
+		console.log("sendLog aleady json link");
+		console.log(json_data["link"]);
+	}
+	console.log(json_data);
 	if(link != ""){
 		$.ajax({
 			type: "post",
@@ -127,8 +271,6 @@ function sendLog(link){
 			data: json_data,
 			success : function(){
 				console.log("success");
-				//now do the redirect
-				// window.location = json_data["link"]
 			},
 			complete: function(){
 				console.log("complete");
@@ -138,18 +280,21 @@ function sendLog(link){
 			}
 		});
 	}
-
-	if(link == ""){
+	else{
 		console.log("fail sending");
 	}
 
+	// check flush queue or not
+	if(flush){
+		sendAllLog(link);
+	}
 
 }
 
 $(document).ready(function() {
-    // init local storage
+	// init local storage
 	if (typeof(Storage) !== "undefined") {
-		
+
 		load_count = Number(localStorage.getItem("load_count"));
 		user_key = localStorage.getItem("user_key");
 
@@ -168,17 +313,11 @@ $(document).ready(function() {
 		load_count += 1;
 		localStorage.setItem("load_count", load_count);
 
-        console.log("log init3");
-        var json_data = localStorage.getItem("json_data");
-	    json_data = JSON.parse(json_data);
-        if( json_data === null ){
-            makeLog(link=document.location.href, init=true);
-//            var json_data = makeLog(document.location.href,true);
-//            localStorage.setItem("json_data", JSON.stringify(json_data));
-            console.log("log init complete");
-        }
-        // init, send visit log
-	    sendLog(document.location.href);
+		if(isEmptyQueue()){
+			makeLog(link=document.location.href, init=true);
+		}
+		// init, send visit log
+		sendLog(link=document.location.href,flush=true);
 
 	} else {
 		console.log("not support");
@@ -186,76 +325,64 @@ $(document).ready(function() {
 });
 
 
+function tagClickListner(event, arg_this, tag) {
+	console.log("DOMListner tag " + tag);
+	if (Number(Date.now() - oldTime_click) > click_listen_milli_sec_threshold) {
+
+		var tagClass = String($(arg_this).attr("class"));
+		tagClass = tagClass !== 'undefined' ? tagClass : "";
+
+		var tagId = String($(arg_this).attr("id"));
+		tagId = tagId !== 'undefined' ? tagId : "";
+
+		var tagName = String($(arg_this).attr("name"));
+		tagName = tagName !== 'undefined' ? tagName : "";
+
+		// var tagReactid = String($(arg_this).attr("reactid"));
+		// tagReactid = tagReactid !== 'undefined' ? tagReactid : "";
+
+		var tagDataReactid = String($(arg_this).attr("data-reactid"));
+		tagDataReactid = tagDataReactid !== 'undefined' ? tagDataReactid : "";
+
+		var s = ";";
+		tagInfo = tag + s + tagClass + s + tagId + s + tagName + s + tagDataReactid;
+
+		if ($(arg_this).attr("href")) {
+			console.log("DOMListner attribute href");
+			makeLog(e = window.event, link = $(arg_this).attr("href"), init = false);
+		}
+		else if ($(arg_this).attr("data-reactid") || $(arg_this).attr("reactid")) {
+			console.log("DOMListner attribute reactids");
+			makeLog(e = window.event, link = tagInfo, init = false);
+		}
+		else{
+			console.log("DOMListner attribute else");
+			console.log(tagInfo);
+			makeLog(e = window.event, link = tagInfo, init = false);
+		}
+	}
+	oldTime_click = Date.now();
+}
+
 
 // click event listener
 var oldTime_click = Date.now();
 var click_listen_milli_sec_threshold = 100;
 $(document).bind('DOMSubtreeModified', function() {
-    // click event listener
+	// click event listener
 	$('a').click(function(e) {
-		console.log("a");
-        currentTime_click = Date.now();
-        if(Number(currentTime_click - oldTime_click) > click_listen_milli_sec_threshold) {
-            if ($(this).attr("href")) {
-                //prevent the redirect;
-                // e.preventDefault();
-                //do your tracking
-                // sendLog($(this).attr("href"));
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("data-reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else {
-                console.log("else1");
-            }
-
-        }
-        oldTime_click = Date.now();
+		tagClickListner(window.event, this, 'a');
 	});
 	$('span').click(function(e) {
-		console.log("span");
-        currentTime_click = Date.now();
-        if(Number(currentTime_click - oldTime_click) > click_listen_milli_sec_threshold) {
-            if ($(this).attr("href")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("data-reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else {
-                console.log("else2");
-            }
-        }
-        oldTime_click = Date.now();
+		tagClickListner(window.event, this, 'span');
 	});
 	$('button').click(function(e) {
-		console.log("button");
-        currentTime_click = Date.now();
-        if(Number(currentTime_click - oldTime_click) > click_listen_milli_sec_threshold) {
-            if ($(this).attr("href")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("data-reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else if ($(this).attr("reactid")) {
-                makeLog(e = window.event, link = $(this).attr("href"), init = false);
-            }
-            else {
-                console.log("else3");
-            }
-        }
-        oldTime_click = Date.now();
+		tagClickListner(window.event, this, 'button');
+	});
+	$('input').click(function(e) {
+		tagClickListner(window.event, this, 'input');
 	});
 });
-
 
 // url change listener
 var urlTracker = document.location.href;
@@ -268,26 +395,17 @@ $(document).bind('DOMSubtreeModified', function() {
 			console.log("page modified!");
 			console.log(urlTracker);
 			console.log(document.location.href);
-			sendLog(document.location.href);
+			sendLog(link=document.location.href, flush=true);
+		}
+		oldTime = Date.now();
+	}
+	else{
+		currentTime = Date.now();
+		if((Number(currentTime - oldTime)) > url_change_milli_sec_threshold) {
+			console.log("single page DOMSubtreeModified");
+			console.log(urlTracker);
+			sendLog(link=document.location.href, flush=true);
 		}
 		oldTime = Date.now();
 	}
 });
-
-
-// old and testing code
-// $(document).ready(function() {
-// 	$("*").click(function(e) {
-// 		//prevent the redirect;
-// 		e.preventDefault();
-// 		//do your tracking
-// 		sendLog($(this).attr("href"));
-// 	});
-// });
-
-//script import code
-// $.getScript('http://0.0.0.0:4000/js/clickTracker.js');
-
-// var jq = document.createElement('script');
-// jq.src = "http://0.0.0.0:4000/js/clickTracker.js";
-// document.getElementsByTagName('head')[0].appendChild(jq);
